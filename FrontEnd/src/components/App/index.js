d1e1cadd14530b4e5e2632729223ae6c214bbb36
Fabrice DENNEMONT <fabden@gmail.com>
2020-06-06 17:29:36 +0200

    supression fichier exemple dans app index

--- FrontEnd/src/components/App/index.js
+++ FrontEnd/src/components/App/index.js
@@ -2,17 +2,12 @@
 import React from 'react';
 
 // == Import
-import Counter from 'src/containers/Counter';
-import reactLogo from './react-logo.svg';
+
 import './styles.css';
 
 // == Composant
 const App = () => (
-  <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-    <Counter />
-  </div>
+"page accueil"
 );
 
 // == Export
