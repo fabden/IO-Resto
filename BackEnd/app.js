192c2b68e3a453384b4b4ccc3e7600b242a19d64
Fabrice DENNEMONT <fabden@gmail.com>
2020-06-13 14:24:57 +0200

    implementation cors et body parser

---
+++ BackEnd/app.js
@@ -0,0 +1,24 @@
+//import 
+const express =require('express');
+const bodyParser = require('body-parser');
+
+const app=express();
+
+//Cors
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+//body-parser 
+
+  app.use(bodyParser.json());
+
+
+
+
+//export module 
+module.exports =app;
