72bbfc101d59d678599244a23962194d5b5377c0
Fabrice DENNEMONT <fabden@gmail.com>
2020-06-14 10:01:39 +0200

    installe eslint sur backend

--- BackEnd/Route/OrdersRoute.js
+++ BackEnd/Route/OrdersRoute.js
@@ -1,16 +1,14 @@
-//import 
+// import
 
 const express = require('express');
-const { Router } = require('express');
+
 const OrderRouter = express.Router();
 
-//route
+// route
 
 OrderRouter.route('/')
-    .get((req, res) => {console.log("chemin get order ")})
-    .post((req, res) => {console.log("chemin post order")})
-    .delete((req, res) => {console.log("chemin delete order")})
-   
-
+  .get((req, res) => { console.log('chemin get order '); })
+  .post((req, res) => { console.log('chemin post order'); })
+  .delete((req, res) => { console.log('chemin delete order'); });
 
 module.exports = OrderRouter;
--- BackEnd/serveur.js
+++ BackEnd/serveur.js
@@ -1,9 +1,8 @@
-//import 
+// import
 const http = require('http');
-const app =require('../BackEnd/app');
+const app = require('./app');
 
-
-app.set('port',process.env.PORT || 3000);
-const server =http.createServer (app);
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
