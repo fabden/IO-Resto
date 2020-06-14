36595bbbb9572fbb31cf19aa20fc823559ab39df
Fabrice DENNEMONT <fabden@gmail.com>
2020-06-14 11:11:35 +0200

    correction route

--- BackEnd/Route/MenuRoute.js
+++ BackEnd/Route/MenuRoute.js
@@ -1,11 +1,10 @@
 // import
-
 const express = require('express');
+
 
 const MenuRouter = express.Router();
 
 // route
-
 MenuRouter.route('/')
   .get((req, res) => { console.log('chemin get menu '); })
   .post((req, res) => { console.log('chemin post menu '); })
