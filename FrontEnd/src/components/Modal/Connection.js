aea15ea6b8bf3319013de9de3d6bcbe387cf3f8e
Fabrice DENNEMONT <fabden@gmail.com>
2020-06-17 17:55:47 +0200

    ajout label et possition center bootstrap

--- FrontEnd/src/components/Modal/Connection.js
+++ FrontEnd/src/components/Modal/Connection.js
@@ -6,38 +6,24 @@ import React from 'react';
 
 import './styles.css';
 
-
-
 // == Composant
 const Connection = () => (
 
-<div>
-    <div className='row justify-content-center'>
-        <div className='text-center col-12'>
-            <input className='col-4'></input>
+<div className='row justify-content-center'>
+    <form className='row m-2'>
+        <div className='col-4  '>
+            <label for="fname">Login:</label><br />
+            <label for="lname">Mots de passe:</label>
         </div>
-    </div>
-    <div className='row justify-content-center'>
-        <div className='text-center col-12'>
-            <input className='col-4'></input>
-        </div>
-    </div>
-    <div className='row justify-content-center'>
-        <div className='text-center col-12'>
-            <input className='col-4'></input>
-        </div>
-    </div>
-    <div className='row justify-content-center'>
-        <div className='text-center col-12'>
-            <input className='col-4'></input>
-        </div>
-    </div>
-
-
-
+        <div className='col-7 '>
+            <input type="text" id="fname" name="fname" />
+            <input type="text" id="lname" name="lname" />
+        </div>     
+    </form>
+    <input type="submit" value="Connection" />
 </div>
 
-);
+);  
 
 // == Export
 export default Connection;
