3110707f04f4c3e8e2b0bbf78f5d904a2f47743a
Fabrice DENNEMONT <fabden@gmail.com>
2020-07-11 10:05:20 +0200

    suppression dossier model

--- BackEnd/Model/OderModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const OderSchema = mongoose.Schema({
-  idUser: { type: String, required: true },
-
-});
-
-module.exports = mongoose.model('Menus', OderSchema);
